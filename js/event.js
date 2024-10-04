7dc3ae7ec261df941de174dd2a7da90d9f744e68
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-10-04 16:48:59 +0300

    done

--- js/event.js
+++ js/event.js
@@ -1,18 +1,65 @@
-var mainH1 = document.getElementById("mainH1");
-var users = document.getElementById("users");
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
+/*var mainH1 = document.getElementById('mainH1');
+mainH1.addEventListener('click', function(){
+    mainH1.style.color = "red"
+})*/
 
-var userForm = document.getElementById("userForm");
-userForm.addEventListener('submit', function(event){
+var mainH1 = document.getElementById('mainH1');
+var mainH1 = document.getElementById('users');
+var fname = document.getElementById('fname');
+var lname = document.getElementById('lname');
+
+var userForm = document.getElementById('userForm');
+userForm.addEventListener('submit', function(event){ //استمع للجدث اللي اسمه سبميت
+    // alert('form submitted')
     event.preventDefault();
-    console.log(fname.value, lname.value);
+    console.log(fname.value, lname.value)
     // create element
-    var firstH1 = document.createElement('h1');
-    firstH1.innerText = "this is fname: "+fname.value +" | this is lname: "+lname.value;
-    console.log(firstH1);
-    firstH1.style.color = "red";
-    // firstH1.style.fontSize = '10px'
+    var firstH1 = document.createElement('H1');
+    firstH1.innerHTML = "this is fname: "+fname.value +" | this is lname" + lname.value;
+    console.log(firstH1)
+    firstH1.style.color = 'red'
     // append or appendChild
     users.appendChild(firstH1);
 })
+
+var number1 = document.getElementById('number1');
+var number2 = document.getElementById('number2');
+var operation = document.getElementById('operation');
+var result = document.getElementById('result');
+function calculate(){
+    if(operation.value == '+'){
+        result.innerHTML = parseInt(number1.value) + parseInt(number2.value);
+        alert("result : " + result.innerHTML)
+    }
+    else if(operation.value == '-'){
+        result.innerHTML = parseInt(number1.value) - parseInt(number2.value);
+        alert("result : " + result.innerHTML)
+    }
+    else if(operation.value == '*'){
+        result.innerHTML = parseInt(number1.value) * parseInt(number2.value);
+        alert("result : " + result.innerHTML)
+    }
+    else if(operation.value == '/'){
+        result.innerHTML = parseInt(number1.value) / parseInt(number2.value);
+        alert("result : " + result.innerHTML)
+    }
+}
+
+
+var primeNumber = document.getElementById('primeNumber');
+var result2 = document.getElementById('result2');
+function isPrime(){
+    console.log(primeNumber.value)
+    // var isPrime = true;
+    for(var i=2; i<parseInt(primeNumber.value); i++){
+        if(primeNumber.value % i == 0){
+        // isPrime = false;
+        result2.innerText = "this is not a prime number";
+        break;
+        }
+        else{
+            // isPrime = true;
+            result2.innerText = "this is a prime number";
+        }
+    }
+}
